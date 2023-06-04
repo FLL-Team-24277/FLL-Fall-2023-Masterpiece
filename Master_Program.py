a8b4f7cf62ed330032b605cf4dd0af792d9504e2
Skip Morrow <skip@pelorus.org>
2023-06-04 07:48:13 -0400

    renamed master_program; added comments

--- Master_Program.py
+++ Master_Program.py
@@ -12,19 +12,21 @@ br = base_robot.BaseRobot()
 
 
 # Run the missions depending on what color is seen here
-validColorList = [base_robot.Color.BLUE,
-                  base_robot.Color.CYAN,
-                  base_robot.Color.GREEN,
-                  base_robot.Color.MAGENTA,
-                  base_robot.Color.ORANGE,
-                  base_robot.Color.RED,
-                  base_robot.Color.VIOLET,
-                  base_robot.Color.WHITE]
+validColorList = [
+    base_robot.Color.BLUE,  # mission one
+    base_robot.Color.CYAN,  # mission two
+    base_robot.Color.GREEN,  # mission three
+    base_robot.Color.MAGENTA,
+    base_robot.Color.ORANGE,
+    base_robot.Color.RED,
+    base_robot.Color.VIOLET,
+    base_robot.Color.WHITE,
+]
 while True:
     while True:
         # Inner loop checks to see what color attachment is installed
         # and provide visual feedback
-        # It then checks to see if a button is pressed. If it is,
+        # It then checks to see if the left button is pressed. If it is,
         # break out of the loop and execute the mission associated
         # with that color
         curColor = br.GetAttachmentColor()
@@ -35,16 +37,20 @@ while True:
             br.hub.display.icon(base_robot.Icon.SAD)
             br.hub.light.off()
 
-        if base_robot.Button.RIGHT in br.hub.buttons.pressed():
-            break
+        # The right button is the abort button. If it is being pressed,
+        # don't execute any other programs. Just stay right here.
+        while base_robot.Button.RIGHT in br.hub.buttons.pressed():
+            pass
 
+        # The left button is the launch button. If it is pressed, break
+        # out of the inner loop and execute the mission associated with
+        # the color detected.
         if base_robot.Button.LEFT in br.hub.buttons.pressed():
             break
 
-    # Outer loop. When we get here, it's because we pressed a button
+    # Outer loop. When we get here, it's because we pressed the left button
     # and broke out of the inner loop. Now execute the mission for the
     # color of the attachment. When the mission is done executing,
     # loop back into the inner loop and do it all again
-    if (br.GetAttachmentColor() == base_robot.Color.GREEN):
+    if br.GetAttachmentColor() == base_robot.Color.GREEN:
         Skip_Mission1_ready_for_master.Run(br)
-
