cfa7e8cb411639c3f88cec2dddce2d8fb3f248e2
CJboyyy <jacksonci@icloud.com>
2023-06-26 17:04:30 -0400

    Cleveland's mission thing

---
+++ cleveland_base_robot.py
@@ -0,0 +1,48 @@
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.parameters import Port, Direction, Axis, Side, Stop, Color, Button, Icon
+from pybricks.robotics import GyroDriveBase
+from pybricks.hubs import PrimeHub
+from pybricks.tools import wait
+
+
+PI = 3.14159
+
+TIRE_DIAMETER = 56  # mm
+AXLE_TRACK = 103  # distance between the wheels, mm
+
+STRAIGHT_SPEED = 400  # normal straight speed for driving, mm/sec
+STRAIGHT_ACCEL = 600  # normal acceleration, mm/sec^2
+TURN_RATE = 150  # normal turning rate, deg/sec
+TURN_ACCEL = 360  # normal turning acceleration, deg/sec^2
+
+
+class BaseRobot:
+    """
+    A collection of methods and Spike Prime objects for FLL Team 24277. \
+    Uses pybricks for most functionality.
+
+    Example:
+
+    >>> import base_robot
+    >>> br = base_robot.BaseRobot()
+    >>> br.Drive(400) #400mm
+    >>> br.GyroTurn(90) #90 deg to the right
+    """
+
+    def __init__(self):
+        self.hub = PrimeHub(top_side=Axis.Z, front_side=-Axis.Y)
+        self._version = "0.1 05/19/2023"
+        self.leftMotor = Motor(Port.E, Direction.COUNTERCLOCKWISE)
+        self.rightmotor = Motor(Port.A)
+        self.robot = GyroDriveBase(self.leftMotor, self.rightmotor, TIRE_DIAMETER, AXLE_TRACK )
+
+    # TODO: Make all of these abortable
+    def GyroTurn(self, angle):
+        pass
+
+    def Drive(self, distance, then=Stop.HOLD, wait=True):
+        self.robot.straight(distance * 10)
+
+    def DriveTank(self, leftMotorSpeed, rightMotorSpeed, measurement, units="mm"):
+        pass
+
