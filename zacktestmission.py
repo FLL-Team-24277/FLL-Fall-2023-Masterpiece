8b25a0b36c81e8f2f953aea82cbeba852f0e3eec
Invisible27 <zackharper10@gmail.com>
2023-07-06 16:59:25 -0400

    I added new test file

---
+++ zacktestmission.py
@@ -0,0 +1,11 @@
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port, Direction
+from pybricks.robotics import GyroDriveBase
+
+
+leftmotor = Motor(Port.E, Direction.COUNTERCLOCKWISE)
+rightmotor = Motor(Port.A)
+
+robot = GyroDriveBase(leftmotor, rightmotor, 56, 103)
+
+robot.straight(100)
