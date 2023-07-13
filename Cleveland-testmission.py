ee037026e9eb9c537251f98b9f181b4ad4284898
CJboyyy <jacksonci@icloud.com>
2023-07-13 16:58:52 -0400

    worked on the base robot

--- Cleveland-testmission.py
+++ Cleveland-testmission.py
@@ -1,17 +1,9 @@
-# from pybricks.pupdevices import Motor
-# from pybricks.parameters import Port, Direction
-# from pybricks.robotics import GyroDriveBase
-
-
-# leftmotor = Motor(Port.E, Direction.COUNTERCLOCKWISE)
-# rightmotor = Motor(Port.A)
-    
-# robot = GyroDriveBase(leftmotor, rightmotor, 56, 103)
-
-# robot.straight(1000)
-
 import cleveland_base_robot
 
 br = cleveland_base_robot.BaseRobot()
 
-br.GyroTurn(-60)
+# br.GyroTurn(-60)
+# br.GyroDrive(-150,50)
+# br.GyroTurn(180)
+# br.GyroDrive(150,50)
+br.rightAttachmentMotor(50,180)
--- cleveland_base_robot.py
+++ cleveland_base_robot.py
@@ -10,7 +10,7 @@ PI = 3.14159
 TIRE_DIAMETER = 56  # mm
 AXLE_TRACK = 103  # distance between the wheels, mm
 
-STRAIGHT_SPEED = 400  # normal straight speed for driving, mm/sec
+STRAIGHT_SPEED = 600  # normal straight speed for driving, mm/sec
 STRAIGHT_ACCEL = 600  # normal acceleration, mm/sec^2
 TURN_RATE = 150  # normal turning rate, deg/sec
 TURN_ACCEL = 360  # normal turning acceleration, deg/sec^2
@@ -35,16 +35,20 @@ class BaseRobot:
         self.leftDriveMotor = Motor(Port.E, Direction.COUNTERCLOCKWISE)
         self.rightDriveMotor = Motor(Port.A)
         self.robot = GyroDriveBase(self.leftDriveMotor, self.rightDriveMotor, TIRE_DIAMETER, AXLE_TRACK )
+        self.robot.settings(STRAIGHT_SPEED, STRAIGHT_ACCEL, TURN_RATE, TURN_ACCEL)
         self.leftAttachmentMotor = Motor(Port.B)
         self.rightAttachmentMotor = Motor(Port.D)
         self.colorSensor = ColorSensor(Port.F)
     
-    def GyroTurn(self, angle):
-        self.robot.turn(angle)
+    def GyroTurn(self, angle, then=Stop.HOLD, wait=True):
+        self.robot.turn(angle, then, wait)
 
     def GyroDrive(self, distance, speed = STRAIGHT_SPEED, then=Stop.HOLD, wait=True):
-        self.robot.straight(distance * 10)
+        self.robot.settings(speed, STRAIGHT_ACCEL, TURN_RATE, TURN_ACCEL)
+        self.robot.straight(distance, then, wait)
 
     def DriveTank(self, leftMotorSpeed, rightMotorSpeed, measurement, units="mm"):
         pass
 
+    def MoveRightAttachmentMotor(self, speed, angle, then=Stop.HOLD, wait=True):
+        self.rightAttachmentMotor.run_angle(speed, angle, then, wait)
