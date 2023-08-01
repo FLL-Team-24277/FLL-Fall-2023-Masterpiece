b21335aacc67657c6e6b96bebbd3b7d5e14b3706
Coach Team 24277 <skip+fllgit@pelorus.org>
2023-08-01 07:45:35 -0400

    Comments

--- colorTest.py
+++ colorTest.py
@@ -6,9 +6,4 @@ while True:
     curHsv = br.colorSensor.hsv(True)
     curCol = br.colorSensor.color()
     print(str(curHsv) + ": " + str(br.myColor2DefaultColorDict[curCol]))
-    # curColor = br.colorSensor.color()
-    # print(curColor)
-    # br.hub.light.on(br.myColor2DefaultColorDict[curColor])
-
-    # print (str(curCol))
     wait(500)
