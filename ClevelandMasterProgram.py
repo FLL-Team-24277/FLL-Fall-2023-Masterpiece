8c133a80298817ceb0a82d182397937a8d1ac6dc
Coach Team 24277 <skip+fllgit@pelorus.org>
2023-08-03 11:58:10 -0400

    Fixed colors

--- ClevelandMasterProgram.py
+++ ClevelandMasterProgram.py
@@ -9,25 +9,23 @@ col=br.colorSensor.color()
 while True:
     while True: 
         col=br.colorSensor.color()
-        print(col)
-        if (str(col)=="Color.SENSOR_NONE"): # no color detected
+        # print(col)
+        if (col==Color.SENSOR_NONE): # no color detected
             br.hub.display.icon(Icon.SAD)
             br.hub.light.on(Color.RED)
-            # br.hub.light.blink(Color.RED, [100, 100])
         else:
             br.hub.display.icon(Icon.HAPPY)
             br.hub.light.on(br.myColor2DefaultColorDict[col])
-            # print(col)
         
         wait(250)
         pressed=br.hub.buttons.pressed()
         if (Button.LEFT in pressed):
             break
     
-    if(col==br.myCustomColors[0]): #Green
+    if(col==Color.SENSOR_GREEN):
         clev_test.Run(br)
 
-    if(col==br.myCustomColors[1]): # Red
-        print("Launching the Red mission")
+    if(col==Color.SENSOR_BLUE):
+        print("Launching the Blue mission")
     
 
