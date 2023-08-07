713e7379bb791d8ad886d5f5d48d82ca92f08a6f
Coach Team 24277 <skip+fllgit@pelorus.org>
2023-08-07 06:38:40 -0400

    Comments

--- cleveland_base_robot.py
+++ cleveland_base_robot.py
@@ -42,18 +42,21 @@ class BaseRobot:
 
         self.colorSensor = ColorSensor(Port.F)
 
-        # HSV values were found by testing
-        Color.SENSOR_GREEN = Color(h=156, s=66, v=66)
-        Color.SENSOR_RED = Color(h=353, s=82, v=92)
-        Color.SENSOR_YELLOW = Color(h=60, s=50, v=100)
-        Color.SENSOR_BLUE = Color(h=216, s=84, v=83)
-        Color.SENSOR_MAGENTA = Color(h=339, s=71, v=81)
+        # HSV values were found by testing. Default h-values are provided
+        # in comments. Theoretically, the farther apart the h-values are, the 
+        # less likely two colors can get "confused"
+        Color.SENSOR_GREEN = Color(h=156, s=66, v=66) # h=120
+        Color.SENSOR_RED = Color(h=353, s=82, v=92) # h=0
+        Color.SENSOR_YELLOW = Color(h=60, s=50, v=100) # h=60
+        Color.SENSOR_BLUE = Color(h=216, s=84, v=83) # h=240
+        Color.SENSOR_MAGENTA = Color(h=339, s=71, v=81) # h=300
         Color.SENSOR_WHITE = Color(h=0, s=0, v=100)
-        Color.SENSOR_ORANGE = Color(h=13, s=73, v=100)
+        Color.SENSOR_ORANGE = Color(h=13, s=73, v=100) # h=30
         Color.SENSOR_DARKGRAY = Color(h=192, s=21, v=64)
         Color.SENSOR_NONE = Color(h=170, s=26, v=15)
 
-        # Put the custom colors in a list
+        # Put the custom colors in a list. Best practice is to only use
+        # colors that we are using for actual missions.
         self.sensorColors = [
             Color.SENSOR_GREEN,
             # Color.SENSOR_RED,
