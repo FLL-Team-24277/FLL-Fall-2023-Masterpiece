099e2d0b5a845156641923b1b1440086c4b8631e
BuildTools <unconfigured@null.spigotmc.org>
2023-08-10 16:31:40 -0400

    hi

--- ClevelandMasterProgram.py
+++ ClevelandMasterProgram.py
@@ -9,20 +9,20 @@ col = br.colorSensor.color()
 while True:
     while True:
         col = br.colorSensor.color()
-        # print(col)
-        if col == Color.SENSOR_NONE:  # no color detected
+        # The first thing this program does is it detects what color is being help up to the robot
+        if col == Color.SENSOR_NONE:  #  If no color is detected, then it will display a sad face
             br.hub.display.icon(Icon.SAD)
             br.hub.light.on(Color.RED)
-        else:
+        else: #  If a color is detected, then it will display a happy face
             br.hub.display.icon(Icon.HAPPY)
             br.hub.light.on(br.myColor2DefaultColorDict[col])
 
         wait(250)
         pressed = br.hub.buttons.pressed()
-        if Button.LEFT in pressed:
+        if Button.LEFT in pressed: #  When the left button is pressed, it will break out of the loop
             break
 
-    if col == Color.SENSOR_GREEN:
+    if col == Color.SENSOR_GREEN: # It will now launch the mission coresponding to the color
         clev_test.Run(br)
 
     if col == Color.SENSOR_BLUE:
