508cf7c1ee6a2c3dcfa55d7feff5d2317a2c793f
Invisible27 <zackharper10@gmail.com>
2023-08-10 16:53:36 -0400

    Sample

--- sample_mission.py
+++ sample_mission.py
@@ -5,23 +5,10 @@ from base_robot import *
 # such as how it should be loaded with anything, arm positions, etc.
 br = BaseRobot()
 
-# br.DriveTank(550, 550, 100, "mm")
-# br.WaitForLeftButtonPress()
-# br.leftAttachmentMotor.run_angle(1110, 720, wait=False)
-# br.rightAttachmentMotor.run_angle(1110, 720)
-# br.WaitForSeconds(1)
-br.GyroTurn(-65)
-# br.Drive(-30)
-# br.WaitForSeconds(1)
-# br.GyroTurn(45)
-# br.WaitForLeftButtonPress()
-# br.leftAttachmentMotor.run_time(550, 2000)
-# br.GyroTurn(-45)
-# br.DriveTank(200, -200, 110, "mm")
-# br.DriveTank(70, 70, 30, "cm")
-# br.Drive(30)
-# br.GyroTurn(650)
-# br.WaitForSeconds(.2)
-# br.GyroTurn(-65)
+
+br.GyroDrive(150)
+br.WaitForMillis(500)
+br.GyroTurn(-93)
+
 
 # Run(base_robot.BaseRobot())
