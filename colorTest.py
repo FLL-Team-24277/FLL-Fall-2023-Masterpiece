ca9273a9853faf0bda3716d6376e2a612f59a5c7
Skip Morrow <skip@pelorus.org>
2023-08-12 19:40:11 -0400

    Comments

--- colorTest.py
+++ colorTest.py
@@ -1,5 +1,8 @@
 from base_robot import *
 
+# This program is just for testing the values seen by the robot
+# for different colors as seen by the light sensor, and what it
+# is matched to by the BaseRobot class
 br = BaseRobot()
 
 while True:
