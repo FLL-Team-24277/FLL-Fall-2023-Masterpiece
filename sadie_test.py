90c89f43d63442a349d0ecf446970201c7968102
sadie <szborowski2030@norfolkcollegiate.org>
2023-09-13 16:49:42 -0400

    just starting

--- sadie_test.py
+++ sadie_test.py
@@ -12,9 +12,13 @@ from base_robot import *
 def Run(br: BaseRobot):
     #   Your mission code goes here, step-by-step
     # It MUST be indented just like the lines belowL
-    br.leftAttachmentMotor.run_angle(250, 390)  # speed 200, 180 degrees
-
-
+   
+    br.GyroDrive(60,500)
+    br.leftAttachmentMotor.run_angle (100,40)
+    br.GyroDrive(20,100)
+    br.leftAttachmentMotor.run_angle (-300, 320)  # speed 200, 180 degrees
+    #br.WaitForMillis(100)
+    br.GyroDrive(-60,500)
 # If running this program directly (not from the master program), this is
 # how we know it is running directly. In which case, this method will
 # create a BaseRobot and run the Run(br) method above.
