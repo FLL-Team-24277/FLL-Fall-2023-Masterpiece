2eacba0ddec582c025ea24eb4ab2e3848b86a1d8
GiosToast <gsykes2030@norfolkcollegiate.org>
2023-09-28 16:48:27 -0400

    quick save

--- giovanni-test.py
+++ giovanni-test.py
@@ -11,21 +11,13 @@ from base_robot import *
 # "Run(br)" method.
 def Run(br: BaseRobot):
     #   Your mission code goes here, step-by-step
-    # It MUST be indented just like the lines below
-    br.GyroDrive(445, 500)
-    br.GyroTurn(57)
-    br.GyroDrive(90)
-    br.leftAttachmentMotor.run_angle(400, 320)
-    br.GyroDrive(-175, 500)
-    br.leftAttachmentMotor.run_angle(400,-320)
-    br.GyroTurn(-75)
-    br.GyroDrive(-380,500)
-
+     #It MUST be indented just like the lines below
+  br.GyroDrive(200, 500)
 # If running this program directly (not from the master program), this is
 # how we know it is running directly. In which case, this method will
 # create a BaseRobot and run the Run(br) method above.
 # In other words, keep these three lines at the bottom of your code and
 # everything will be fine.
-if __name__ == "__main__":
+ if __name__ == "__main__":
     br = BaseRobot()
     Run(br)
