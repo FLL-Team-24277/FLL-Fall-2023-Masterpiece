963a260529a12e0d1fb3690af1f057df457c153e
Zack <zharper2029@norfolkcollegiate.org>
2023-10-09 16:51:07 -0400

    Made more missions work

--- zack_missions.py
+++ zack_missions.py
@@ -1,14 +1,23 @@
 from base_robot import *
 
 def Run(br: BaseRobot):
-    # sound Mixer
     # Positive numbers lower the arm; negative raises
     # Positive makes the robot go forward/ Negative makes the robot go backward
-    br.leftAttachmentMotor.run_angle(978,160,wait=False)
-    br.GyroDrive(370)
-    br.leftAttachmentMotor.run_angle(195,-150,wait=False)
-    br.GyroDrive(50,50)
-    br.GyroDrive(-365)
+    # 3D Cinema
+    br.GyroDrive(310,950)
+    br.leftAttachmentMotor.run_angle(300,300)
+    br.GyroDrive(-300,700)
+    # Sound Mixer
+    br.WaitForButton(Button.LEFT)
+    # br.leftAttachmentMotor.run_until_stalled(500,duty_limit=50)
+    br.GyroDrive(370,950)
+    br.GyroDrive(50,75)
+    br.GyroDrive(-25,75)
+    br.leftAttachmentMotor.run_angle(195,-300,wait=False)
+    br.GyroDrive(55,60)
+    # Theater Scene Change
+    br.GyroDrive(-200,700)
+    br.GyroTurn(-45)
 
 
 if __name__ == "__main__":
