7f58f463d0f0919453da90e984ec676430b3eab6
Zack <zharper2029@norfolkcollegiate.org>
2023-10-12 16:50:13 -0400

    mission works

--- zack_missions.py
+++ zack_missions.py
@@ -4,22 +4,22 @@ def Run(br: BaseRobot):
     # Positive numbers lower the arm; negative raises
     # Positive makes the robot go forward/ Negative makes the robot go backward
     # 3D Cinema
-    # br.GyroDrive(315,950)
-    # br.leftAttachmentMotor.run_until_stalled(700)
-    # br.GyroDrive(-300,700)
+    br.GyroDrive(315,950)
+    br.leftAttachmentMotor.run_until_stalled(700)
+    br.GyroDrive(-300,700)
     # # Sound Mixer
-    # br.WaitForButton(Button.LEFT)
-    # br.GyroDrive(370,950)
-    # br.GyroDrive(50,75)
-    # br.GyroDrive(-25,75)
-    # br.leftAttachmentMotor.run_angle(195,-300,wait=False)
-    # br.GyroDrive(55,60)
-    # # Theater Scene Change
-    # br.GyroDrive(-150,700)
-    # br.GyroTurn(-40)
-    # br.GyroDrive(485,700)
-    # br.GyroTurn(-40)
-    # br.GyroDrive(-125,500)
+    br.WaitForButton(Button.LEFT)
+    br.GyroDrive(370,950)
+    br.GyroDrive(50,75)
+    br.GyroDrive(-25,75)
+    br.leftAttachmentMotor.run_angle(195,-300,wait=False)
+    br.GyroDrive(55,60)
+    # Theater Scene Change
+    br.GyroDrive(-150,700)
+    br.GyroTurn(-40)
+    br.GyroDrive(485,700)
+    br.GyroTurn(-40)
+    br.GyroDrive(-125,500)
     br.GyroDrive(250,700)
     br.GyroDrive(-175,700)
     
