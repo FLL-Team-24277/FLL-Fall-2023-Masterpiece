ea9a4e0a33ede4d59229e975e916effaa9714b81
carter <crobb2030@norfolkcollegiate.org>
2023-10-21 12:54:31 -0400

    polish-up

--- carter_test.py
+++ carter_test.py
@@ -8,31 +8,46 @@ from base_robot import *
 # When we run this program from the master program, we will call this
 # "Run(br)" method.
 def Run(br: BaseRobot):
-    br.WaitForButton(Button.LEFT)
-    br.GyroDrive(420)  # drive 420 mm
-    br.GyroTurn(-50)  # turn -50 degrees
-    br.GyroDrive(700)  # drive 700 mm
-    br.GyroTurn(20)  # turn 20 degrees
-    br.GyroDrive(50)  # drive 50 mm
-    br.GyroDrive(-240)  # drive -240 mm
-    br.GyroTurn(-37)  # turn -37 degrees
-    br.GyroDrive(440)  # drive 440 mm
-    br.GyroTurn(35)  # turn 435 degrees
-    br.GyroDrive(200)  # drive 200 mm
-    br.GyroTurn(20)  # turn 20 degrees
-    br.rightAttachmentMotor.run_angle(1000, 300)
-    br.GyroDrive(-250)  # drive -250 mm
-    br.GyroTurn(-15)  # turn -15 degrees
-    br.GyroDrive(200)  # drive 200 mm
-    br.GyroTurn(-50)  # turn -50 degrees
-    br.GyroDrive(150, 150)  # drive 150, 150 mm
-    br.leftAttachmentMotor.run_angle(-1000, 300)
-    br.GyroDrive(-40)  # drive -40,  150 mm
+     br.GyroDrive(420)  # drive 420 mm
+     br.GyroTurn(-50)  # turn -50 degrees
+     br.GyroDrive(700)  # drive 700 mm
+     br.GyroTurn(20)  # turn 20 degrees
 
-    # br.GyroTurn(80)  # turn 80 degrees
-    # br.GyroDrive()
-    # br.leftAttachmentMotor.run_angle(1000, 300)
-    # br.GyroDrive(-50)  # drive -50 mm
+     # enter the museum
+     br.GyroDrive(65)  # drive 700 mm
+     br.GyroDrive(-255)  # drive -240 mm
+
+     # Drive to skateboard
+     br.GyroTurn(-37)  # turn -37 degrees
+     br.GyroDrive(480)  # drive 480 mm
+     br.GyroTurn(40)  # drive 40 mm
+
+     # enter the skateboard
+     br.GyroDrive(170)
+     br.rightAttachmentMotor.run_angle(1000, 300)
+     br.GyroDrive(-200)
+     br.GyroTurn(45) 
+     br.Curve(radius=-350, angle=-100)
+     br.GyroDrive(-500, -977)
+
+#     base to popcorn
+#     br.WaitForButton(Button.LEFT)
+#     br.GyroDrive(50) # drive 50 mm
+
+
+#     Design one(skateboard to popcorn)
+#     br.GyroDrive(-250)  # drive -250 mm
+#     br.GyroTurn(-15)  # turn -15 degrees
+#     br.GyroDrive(200)  # drive 200 mm
+#     br.GyroTurn(-50)  # turn -50 degrees
+#     br.GyroDrive(300 , 150)  # drive 150, 150 mm
+#     br.leftAttachmentMotor.run_angle(-1000, 300)
+#     br.GyroDrive(-100)  # drive150 mm
+
+# br.GyroTurn(80)  # turn 80 degrees
+# br.GyroDrive()
+# br.leftAttachmentMotor.run_angle(1000, 300)
+# br.GyroDrive(-50)  # drive -50 mm
 
 
 # If running this program directly (not from the master program), this is
