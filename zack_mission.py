fcc61b972da7c986941bb35f68e4e50de33cff07
Zack <zharper2029@norfolkcollegiate.org>
2023-11-02 17:00:53 -0400

    This is so annoying

--- zack_mission.py
+++ zack_mission.py
@@ -53,25 +53,32 @@ def Run(br: BaseRobot):
     br.GyroDrive(428, 900)
     br.GyroTurn(-90)
     br.GyroDrive(58, 900)
-    br.leftAttachmentMotor.run_angle(900, 350)
+    br.leftAttachmentMotor.run_time(speed=900, time=1000)
     br.leftAttachmentMotor.run_time(-900, 1000)
     br.GyroDrive(-75, 900)
 
     # Augmented Reality
 
-    br.GyroTurn(35)
-    br.GyroDrive(375, 900)
-    br.GyroDrive(-25, 500)
-    # br.Curve(40,50)
+    br.GyroTurn(34)
+    br.GyroDrive(324, 900)
+    br.GyroTurn(-34)
+    br.GyroDriveForMillis(700,800)
+    # br.GyroDrive(-5,500)
+    # br.GyroTurn(90)
+    # br.GyroDrive(-185,900)
     # br.leftAttachmentMotor.run_until_stalled(900)
+    # br.DriveAndSteer(500,-20,1000)
+    # br.GyroDrive(-10,300)
+    # br.GyroTurn(45)
+    
+    
+    # Back To Base
 
-    # # Back To Base
-
-    # br.GyroDrive(100, 900)
-    # br.GyroTurn(-45)
-    # br.GyroDrive(400, 900)
-    # br.Curve(275, 60)
-    # br.GyroDrive(300, 900)
+    br.GyroDrive(100, 900)
+    br.GyroTurn(-45)
+    br.GyroDrive(400, 900)
+    br.Curve(275, 60)
+    br.GyroDrive(300, 900)
 
 
 if __name__ == "__main__":
