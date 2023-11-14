09c5b1228f40799bb269dd25f812e524c9f4ab73
Zack <zharper2029@norfolkcollegiate.org>
2023-11-14 16:18:16 -0500

    rj here

--- cad_car_mission.py
+++ cad_car_mission.py
@@ -8,33 +8,33 @@ from base_robot import *
 # When we run this program from the master program, we will call this
 # "Run(br)" method.
 def Run(br: BaseRobot):
-    br.GyroDrive(120, 977)  # drive 60 mm
-    br.GyroTurn(-17)  # turn -12 degrees
-    br.GyroDrive(425, 977)  # drive 420 mm
-    br.GyroTurn(-60)  # turn -50 degrees
-    br.GyroDrive(690, 977)  # drive 700 mm
+    # br.GyroDrive(120, 977)  # drive 60 mm
+    # br.GyroTurn(-17)  # turn -12 degrees
+    # br.GyroDrive(425, 977)  # drive 420 mm
+    # br.GyroTurn(-60)  # turn -50 degrees
+    # br.GyroDrive(690, 977)  # drive 700 mm
 
-    # enter the museum
-    br.GyroTurn(30)  # turn 15 degrees
-    br.GyroDrive(68, 977)  # drive 65 mm
-    br.GyroDrive(-265, 977)  # drive -255 mm
+    # # enter the museum
+    # br.GyroTurn(30)  # turn 15 degrees
+    # br.GyroDrive(68, 977)  # drive 65 mm
+    # br.GyroDrive(-265, 977)  # drive -255 mm
 
-    # drive to skateboard
-    br.GyroTurn(-37)  # turn -37 degrees
-    br.GyroDrive(590, 977)  # drive 490 mm
-    br.GyroTurn(40)  # turn 60 degrees
+    # # drive to skateboard
+    # br.GyroTurn(-37)  # turn -37 degrees
+    # br.GyroDrive(590, 977)  # drive 490 mm
+    # br.GyroTurn(40)  # turn 60 degrees
 
-    # enter base from skateboard
-    br.GyroDrive(130, 977)  # drive 170 mm
-    br.rightAttachmentMotor.run_time(977, 800)
-    br.GyroDrive(-250, 977)  # drive -230 mm
-    br.GyroTurn(55)  # turn 55 degrees
-    br.Curve(radius=-330, angle=-60, speed=800)
-    br.GyroDrive(-800, -977)
+    # # enter base from skateboard
+    # br.GyroDrive(130, 977)  # drive 170 mm
+    # br.rightAttachmentMotor.run_time(977, 800)
+    # br.GyroDrive(-250, 977)  # drive -230 mm
+    # br.GyroTurn(55)  # turn 55 degrees
+    # br.Curve(radius=-330, angle=-60, speed=800)
+    # br.GyroDrive(-600, -977)
 
     # base to popcorn
     br.WaitForButton(Button.LEFT)
-    br.GyroDrive(380, 977)  # drive 340 mm
+    br.GyroDrive(450, 977)  # drive 340 mm
     br.GyroDriveForMillis(1000, -990)
 
 
