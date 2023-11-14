8139bafaf2c3ac9a8f17015e88557a0f9de4f27b
sadie <szborowski2030@norfolkcollegiate.org>
2023-11-14 16:23:47 -0500

    making better

--- sadie_mission.py
+++ sadie_mission.py
@@ -12,7 +12,7 @@ from base_robot import *
 def Run(br: BaseRobot):
     #   Your mission code goes here, step-by-step
     # It MUST be indented just like the lines below
-    br.leftAttachmentMotor.run_until_stalled(-990, Stop.COAST, 40)
+    # br.leftAttachmentMotor.run_until_stalled(-990, Stop.COAST, 40)
     br.GyroDrive(325, 990)
     br.WaitForMillis(100)
     br.leftAttachmentMotor.run_angle(990, 90, wait=True)
@@ -21,9 +21,9 @@ def Run(br: BaseRobot):
     br.GyroDrive(-330, 900)
 
     br.WaitForButton(Button.RIGHT)
-    br.GyroDrive(-100, 990)
+    br.GyroDrive(-80, 990)
     br.GyroTurn(-55)
-    br.GyroDrive(-360, 990)
+    br.GyroDrive(-370, 990)
     br.WaitForMillis(300)
     br.GyroDrive(800, 990)
 
