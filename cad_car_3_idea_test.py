bfb9293fc08c04ee54a25745464ecaecb1b5ac40
slcyed <jonaselsass@yahoo.com>
2024-03-28 16:46:32 -0400

    Cad Car

--- cad_car_3_idea_test.py
+++ cad_car_3_idea_test.py
@@ -2,13 +2,7 @@ from base_robot import *
 
 
 def Run(br: BaseRobot):
-    # ACCEL = 350
-    # br.robot.settings(
-    # straight_speed=400,
-    # straight_acceleration=ACCEL,
-    # turn_rate=150,
-    # turn_acceleration=360,
-    # )
+    # Reset Gyro
     br.robot.reset()
     br.rightAttachmentMotor.run_time(-500, 1000, wait=False)
     # br.GyroDrive(1000, 500)
@@ -19,12 +13,14 @@ def Run(br: BaseRobot):
     br.Curve(radius=200, angle=45, speed=977, then=Stop.HOLD)
     br.GyroDrive(-100, 750)
     # # br.robot.use_gyro(True)
-    br.GyroTurn(-45)
+    br.GyroTurn(-50)
     br.GyroDrive(100, speed=977, then=Stop.NONE)
     br.rightAttachmentMotor.run_time(500, 1000, wait=False)
-    br.Curve(radius=750, angle=20, speed=977, then=Stop.NONE)
-    br.GyroDrive(100, speed=977, then=Stop.HOLD)
-    br.GyroDrive(-300, 700)
+    br.Curve(radius=750, angle=38, speed=977)
+    br.GyroDrive(distance=20)
+    br.GyroDrive(distance=-200)
+    # br.GyroDrive(100, speed=977, then=Stop.HOLD)
+    # br.GyroDrive(-300, 700)
     # br.GyroDrive(-395, 977)
     # br.GyroTurn(-45)
     # br.rightAttachmentMotor.run_time(500, 1000, wait=False)
