2e01762465f819aac3dab227d9a6e46a757b553c
Skip Morrow <skip@pelorus.org>
2024-05-05 08:09:23 -0400

    Updates

--- basetest.py
+++ basetest.py
@@ -12,39 +12,39 @@ def Run(br: BaseRobot):
     br.WaitForButton(Button.RIGHT)
     br.TurnInPlace(angle=45)
 
-    br.hub.display.number(1)
-    br.WaitForButton(Button.RIGHT)
-    br.DriveDist(distance=60)
+    # br.hub.display.number(1)
+    # br.WaitForButton(Button.RIGHT)
+    # br.DriveDist(distance=60)
 
-    br.hub.display.number(2)
-    br.WaitForButton(Button.RIGHT)
-    br.WallFollowDist(dist=160, turnRate=15)
+    # br.hub.display.number(2)
+    # br.WaitForButton(Button.RIGHT)
+    # br.WallFollowDist(dist=160, turnRate=15)
 
-    br.hub.display.number(3)
-    br.WaitForButton(Button.RIGHT)
-    br.Curve(radius=400, angle=15)
+    # br.hub.display.number(3)
+    # br.WaitForButton(Button.RIGHT)
+    # br.Curve(radius=400, angle=15)
 
-    br.hub.display.number(4)
-    br.WaitForButton(Button.RIGHT)
-    br.DriveMillis(millis=800)
+    # br.hub.display.number(4)
+    # br.WaitForButton(Button.RIGHT)
+    # br.DriveMillis(millis=800)
 
-    br.hub.display.number(5)
-    br.WaitForButton(Button.RIGHT)
-    br.DriveUntilStalled(stallPct=25, speedPct=25)
+    # br.hub.display.number(5)
+    # br.WaitForButton(Button.RIGHT)
+    # br.DriveUntilStalled(stallPct=25, speedPct=25)
 
-    br.hub.display.number(6)
-    br.WaitForButton(Button.RIGHT)
-    br.MoveAttachmentMotorDegrees(motor=br.rightAttachmentMotor, angle=360)
+    # br.hub.display.number(6)
+    # br.WaitForButton(Button.RIGHT)
+    # br.MoveAttachmentMotorDegrees(motor=br.rightAttachmentMotor, angle=360)
 
-    br.hub.display.number(7)
-    br.WaitForButton(Button.RIGHT)
-    br.MoveAttachmentMotorMillis(motor=br.leftAttachmentMotor, millis=2000)
+    # br.hub.display.number(7)
+    # br.WaitForButton(Button.RIGHT)
+    # br.MoveAttachmentMotorMillis(motor=br.leftAttachmentMotor, millis=2000)
 
-    br.hub.display.number(8)
-    br.WaitForButton(Button.RIGHT)
-    br.MoveAttachmentMotorUntilStalled(
-        motor=br.rightAttachmentMotor, speedPct=100, torquePct=50
-    )
+    # br.hub.display.number(8)
+    # br.WaitForButton(Button.RIGHT)
+    # br.MoveAttachmentMotorUntilStalled(
+    #     motor=br.rightAttachmentMotor, speedPct=100, torquePct=50
+    # )
 
 
 # If running this program directly (not from the master program), this is
